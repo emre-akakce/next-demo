41fdd024099386a92d5209b0d26d6244107661a0
emre-akakce <emre@akakce.com>
2025-01-14 11:19:51 +0300

    add PList component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,33 +1,10 @@
-import Image from "next/image";
-import styles from "./page.module.css";
-
-async function getData() {
-  const res = await fetch('http://localhost:5555/cep-tel', { cache: 'no-store' }); // Avoid caching for dynamic content
-  if (!res.ok) {
-    throw new Error(`Failed to fetch data: ${res.statusText}`);
-  }
-  return res.json();
-}
+import PList from "./components/PList";
 
 export default async function Home() {
-  const data = await getData();
 
   return (
-    <div className="product-list">
-      <h2>İkinci El Cep Telefonu Fırsatları</h2>
-      <div className="product-container">
-        {data.result.category.horizontalProductListContainer.top[0].pgList.map((item: any, index: any) => (
-          <div className="product" key={index}>
-            <img src={item.imageUrl} alt={item.product} />
-            <div className="product-details">
-              <h3>{item.product}</h3>
-              <p><strong>Fiyat:</strong> {item.price}</p>
-              <p><strong>Satıcı:</strong> {item.merchant}</p>
-              <a href={item.url} target="_blank" rel="noopener noreferrer">Ürüne Git</a>
-            </div>
-          </div>
-        ))}
-      </div>
-    </div>
+   <>
+    <PList/>
+   </>
   );
 }
